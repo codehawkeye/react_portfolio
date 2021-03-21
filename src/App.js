157488890e7ee64bc9b02bbd46371bc1f45168dc
Mike Carter <elite.mikecarter@gmail.com>
2021-03-21 14:21:38 -0400

    created components js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import {Container, Grid} from '@material-ui/core';
 import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+
+import Profile from './components/Profile/Profile'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Portfolio from './components/pages/Portfolio/Portfolio'
+import Resume from './components/pages/Resume/Resume'
+
 import './App.css';
+
 
 const App = () => {
     return (
         <Container>
             <Grid container>
-                <Grid item xs={12} sm={12} md={4} lg={3}>123</Grid>
-                <Grid item xs style={{ backgroundColor: 'blue' }}> </Grid>
-            </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor: 'red'}}>
+                 <Profile />
+                  </Grid>
+                 <Grid item xs style={{ backgroundColor: 'blue' }}>
+                   <Header />
+                <Portfolio />
+                <Resume />
+                <Footer/>
+                </Grid>               
+            </Grid>        
         </Container>
     );
 }
---
+++ src/components/pages/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <div>
+            THis is the portfolio
+        </div>
+    )
+}
+
+export default Portfolio
