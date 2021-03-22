2e19b57e1a51cb57fc1512c6f60f332648e0858f
Mike Carter <elite.mikecarter@gmail.com>
2021-03-22 15:07:36 -0400

    adding profile CSS

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 
+import './Profile.css'
+
 
 const Profile = () => {
     return (
         <div className="profile container_shadow">
             <div className='profile_name'>
                 <Typography className='name'>Michael Carter</Typography>
-                <Typography className='name'>Web Developer</Typography>
+                <Typography className='name' >Web Developer</Typography>
             </div>
             <figure className='profile_image'>
                 <img src="" alt="" />
