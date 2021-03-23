7fe7da36ae8423f5547bdaad05cb2a1740285128
Mike Carter <elite.mikecarter@gmail.com>
2021-03-22 22:11:26 -0400

    created timeline js and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container, Grid } from '@material-ui/core';
 
-import Profile from './components/Profile/Profile'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Portfolio from './components/pages/Portfolio/Portfolio'
-import Resume from './components/pages/Resume/Resume'
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Portfolio from './components/pages/Portfolio/Portfolio';
+import Resume from './components/pages/Resume/Resume';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import CustomTimeline from '../Timeline/Timeline';
 
 import './Profile.css'
 
@@ -9,13 +10,13 @@ const Profile = () => {
         <div className="profile container_shadow">
             <div className='profile_name'>
                 <Typography className='name'>Michael Carter</Typography>
-                <Typography className='name' >Web Developer</Typography>
+                <Typography className='title' >Web Developer</Typography>
             </div>
             <figure className='profile_image'>
-                <img src="" alt="" />
+                <img src={require('../../assets/images/profilePic.jpg')} alt='' />
             </figure>
         <div className = 'profile_information' >
-                Insert Timeline
+                <CustomTimeline/>
             <br />
                 <button> my Button</button>
                 </div>             
